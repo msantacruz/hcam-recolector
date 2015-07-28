603f2fac25cc66655c45e11c40570de6ab95d9d4
jorgecardenasvilla <giorgio_24@hotmail.com>
2015-07-28 18:59:25 -0500

    cambio de clave

--- src/ec/gob/iess/casamaquinas/recolector/GestorConexion.java
+++ src/ec/gob/iess/casamaquinas/recolector/GestorConexion.java
@@ -11,7 +11,7 @@ public class GestorConexion {
 		try {
 			Class.forName("org.postgresql.Driver");
 			Connection connection = DriverManager.getConnection(
-					"jdbc:postgresql://localhost:5432/hcam", "postgres", "postgres");
+					"jdbc:postgresql://localhost:5432/hcam", "adminbwhvjhr", "xnw5Px3WqUi6");
 			return connection;	
 		} catch (Exception e) {
 			e.printStackTrace();
